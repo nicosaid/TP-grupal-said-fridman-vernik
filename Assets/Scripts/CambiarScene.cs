1a3fd08b43938e093c4305e1399b8f900f1e40dd
nicosaid <nico.said.r@gmail.com>
2024-06-26 21:07:52 -0600

    panel de notificaciones

---
+++ Assets/Scripts/CambiarScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarScene : MonoBehaviour
+{
+    public void ChangeScene(string comida)
+    {
+        SceneManager.LoadScene(comida);
+    }
+   
+}
