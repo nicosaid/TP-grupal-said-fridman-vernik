7dd68bc24155d85f48f177cdaefc3566bf19acae
nicosaid <nico.said.r@gmail.com>
2024-06-27 10:33:40 -0300

    correcion(comida aleatoria y precios)-HECHO POR FRIDMAN YA QUE SE ME REMPIO LA COMPU

--- Assets/Scripts/comidasScript.cs
+++ Assets/Scripts/comidasScript.cs
@@ -1,44 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class comidasScript : MonoBehaviour
+public class ComidasScript : MonoBehaviour
 {
-    public GameObject[] producto1;
-    public GameObject[] producto2;
-    int productoizquierdo;
-    int productoderecho;
+    public GameObject[] productos;
+    int productoRandom;
+    GameObject producto;
+    Dictionary<GameObject, int> precios = new Dictionary<GameObject, int>();
+    public Text txtPrecio1;
+    public Text txtPrecio2;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < productos.Length; i++)
+        {
+            precios.Add(productos[i], Random.Range(1, 25));
+        }
+        Activate(154.7f, 138.6f, -313.9f);
+        txtPrecio1.text = precios[producto].ToString();
+        Activate(269f, 138.6f, -313.9f);
+        txtPrecio2.text = precios[producto].ToString();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Deactivate();
-        Activate();
-    }
-    void Deactivate()
-    {
-        for (int i = 0; i < producto1.Length; i++)
-        {
-            producto1[i].SetActive(false);
-            producto2[i].SetActive(false);
-        }
-    }
-    void Activate()
-    {
-        productoizquierdo = Random.Range(0, producto1.Length);
-        productoderecho = Random.Range(0, producto2.Length);
-        producto1[productoizquierdo].transform.position = new Vector3(5f, 0f, 0f);
-        producto2[productoderecho].transform.position = new Vector3(-5f, 0f, 0f);
-        producto1[productoizquierdo].SetActive(true);
-        producto1[productoderecho].SetActive(true);
 
+    }
 
+    void Activate(float x, float y, float z)
+    {
+        productoRandom = Random.Range(0, productos.Length);
+        Vector3 posicion = new Vector3(x, y, z);
+        producto = productos[productoRandom];
+        Instantiate(producto, posicion, Quaternion.identity);
     }
 }
-
